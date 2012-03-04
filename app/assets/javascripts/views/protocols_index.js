5001ca269254d2d84e2cbd2eed40c7096ac99b0e
davidsulc <david@davidsulc.com>
2012-03-04 18:13:21 -0500

    use composite view to render index

--- app/assets/javascripts/views/protocols_index.js
+++ app/assets/javascripts/views/protocols_index.js
@@ -1,15 +1,24 @@
-Protocolonel.Views.ProtocolsIndex = Backbone.View.extend({
+Protocolonel.Views.ProtocolsIndex = Support.CompositeView.extend({
   initialize: function() {
   },
 
   render: function () {
+    this.renderLayout();
+    this.renderProtocols();
+    return this;
+  },
+  
+  renderLayout: function () {
     this.$el.html(JST['protocols/index']({ protocols: this.collection }));
+  },
+  
+  renderProtocols: function () {
     var self = this;
     this.collection.each(function(protocol) {
-      var protocolItem = new Protocolonel.Views.ProtocolItem({model: protocol});
-      self.$('table > tbody').append(protocolItem.render().el);
+      var row = new Protocolonel.Views.ProtocolItem({model: protocol});
+      self.renderChild(row);
+      self.$('table > tbody').append(row.el);
     });
-    return this;
   },
 
   leave: function() {
