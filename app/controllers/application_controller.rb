c4b861fcf7afa8831554e531864521399f9e37f7
davidsulc <david@davidsulc.com>
2012-03-05 13:52:59 -0500

    attempt not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :bootstrap_backbone
+  
+  private
+  def bootstrap_backbone
+    @_protocols ||= Protocol.all
+  end
 end
